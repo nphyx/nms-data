a7d2739f0dae71d7f6edf4b3f5673cd5df051431
Justen Robertson <justen@justen.us>
2018-08-18 04:01:37 -0700

    feat: updated elements module to NEXT data, renamed to materials

---
+++ src/materials.js
@@ -0,0 +1,92 @@
+/* @flow */
+import * as util from "./util"
+
+export type Material = {
+  name: string,
+  symbol: string,
+  group: string,
+  value: number
+}
+
+export const array: Array<Material>  = [
+  {name: "Dioxite", symbol: "C02", group: "Localized Mineral", value: 62.0},
+  {name: "Phosphorus", symbol: "P", group: "Localized Mineral", value: 62.0},
+  {name: "Pyrite", symbol: "Py", group: "Localized Mineral", value: 62.0},
+  {name: "Paraffinium", symbol: "Pf", group: "Localized Mineral", value: 62.0},
+  {name: "Uranium", symbol: "U", group: "Localized Mineral", value: 62.0},
+  {name: "Ammonia", symbol: "NH3", group: "Localized Mineral", value: 62.0},
+
+  {name: "Ferrite Dust", symbol: "Fe", group: "Metallic Mineral", value: 14.0},
+  {name: "Pure Ferrite", symbol: "Fe+", group: "Metallic Mineral", value: 28.0},
+  {name: "Magnetized Ferrite", symbol: "Fe++", group: "Metallic Mineral", value: 82.0},
+
+  {name: "Sodium", symbol: "Na", group: "Catalyst Mineral", value: 41.0},
+  {name: "Sodium Nitrate", symbol: "Na+", group: "Catalyst Mineral", value: 82.0},
+
+  {name: "Cobalt", symbol: "Co", group: "Mineral", value: 198.0},
+  {name: "Ionized Cobalt", symbol: "Co+", group: "Mineral", value: 401.0},
+
+  {name: "Carbon", symbol: "C", group: "Organic", value: 12.0},
+  {name: "Condensed Carbon", symbol: "C+", group: "Organic", value: 24.0},
+
+  {name: "Salt", symbol: "NaCl", group: "Mineral", value: 299.0},
+  {name: "Chlorine", symbol: "Cl", group: "Mineral", value: 602.0},
+
+  {name: "Oxygen", symbol: "02", group: "Fuel", value: 34.0},
+  {name: "Di-hydrogen", symbol: "H", group: "Fuel", value: 34.0},
+  {name: "Deuterium", symbol: "D", group: "Fuel", value: 34.0},
+  {name: "Tritium", symbol: "H3", group: "Fuel", value: 6.0},
+
+  {name: "Copper", symbol: "Cu", group: "Stellar", value: 110.0},
+  {name: "Cadmium", symbol: "Cd", group: "Stellar", value: 234.0},
+  {name: "Emeril", symbol: "Em", group: "Stellar", value: 275.0},
+  {name: "Indium", symbol: "In", group: "Stellar", value: 464.0},
+  {name: "Activated Copper", symbol: "Cu+", group: "Stellar", value: 245.0},
+  {name: "Activated Cadmium", symbol: "Cd+", group: "Stellar", value: 450.0},
+  {name: "Activated Emeril", symbol: "Em+", group: "Stellar", value: 696.0},
+  {name: "Activated Indium", symbol: "In+", group: "Stellar", value: 949.0},
+  {name: "Chromatic metal", symbol: "Ch", group: "Stellar", value: 245.0},
+
+  {name: "Silver", symbol: "Ag", group: "Asteroid Mineral", value: 101.0},
+  {name: "Gold", symbol: "Au", group: "Asteroid Mineral", value: 220.0},
+  {name: "Platinum", symbol: "Pt", group: "Asteroid Mineral", value: 55.0},
+
+  {name: "Rare Metal Element", symbol: "Fe*", group: "Concentrated Mineral", value: 4200.0},
+  {name: "TetraCobalt", symbol: "Co*", group: "Concentrated Mineral", value: 6150.0},
+  {name: "Superoxide Crystal", symbol: "O2*", group: "Concentrated Mineral", value: 5100.0},
+  {name: "Carbon Crystal", symbol: "C*", group: "Concentrated Mineral", value: 3600.0},
+  {name: "Destabilized Sodium", symbol: "Na*", group: "Concentrated Mineral", value: 12300.0},
+
+  {name: "Sulphurine", symbol: "Su", group: "Compressed Atmospheric Gas", value: 20.0},
+  {name: "Radon", symbol: "Rn", group: "Compressed Atmospheric Gas", value: 20.0},
+  {name: "Nitrogen", symbol: "N", group: "Compressed Atmospheric Gas", value: 20.0},
+
+  {name: "Cactus Flesh", symbol: "Cc", group: "Agricultural Product", value: 28.0},
+  {name: "Coprite", symbol: "Cr", group: "Agricultural Product", value: 30.0},
+  {name: "Frost Crystal", symbol: "Fr", group: "Agricultural Product", value: 12.0},
+  {name: "Fungal Mould", symbol: "Fm", group: "Agricultural Product", value: 16.0},
+  {name: "Mordite", symbol: "Mo", group: "Agricultural Product", value: 40.0},
+  {name: "Solanium", symbol: "So", group: "Agricultural Product", value: 70.0},
+  {name: "Star Bulb", symbol: "Sb", group: "Agricultural Product", value: 32.0},
+  {name: "Gamma Root", symbol: "Gr", group: "Agricultural Product", value: 16.0},
+  {name: "Marrow Bulb", symbol: "Mb", group: "Netural/Exotic Material", value: 16.0},
+  {name: "Kelp Sac", symbol: "Ke", group: "Neutral/Exotic Material", value: 41.0},
+  {name: "Pugneum", symbol: "Pg", group: "Anomolous Material", value: 138.0}
+]
+
+const unknown: Material = Object.freeze({name: "Unknown", symbol: "", group: "", value: 0.0})
+
+array.forEach(m => Object.freeze(m))
+Object.freeze(array)
+export const set = Object.freeze(new Set(array))
+Object.freeze(unknown)
+
+export const find = (search: any, field: string = "symbol") => util.find(array, search, field) || unknown
+export const filter = (search: any, field: string = "group") => util.filter(array, search, field)
+export const any = (search: any, field: string = "symbol"): Array<Element> => util.any(array, search, field)
+export const uniqueProperties = (field: string) => util.uniqueProperties(array, field)
+
+
+export const groups = Object.freeze(uniqueProperties("group"))
+export const names = Object.freeze(uniqueProperties("name"))
+export const symbols = Object.freeze(uniqueProperties("symbol"))
---
+++ test/materials.js
@@ -0,0 +1,111 @@
+/* @flow */
+import * as materials from "../src/materials"
+import * as util from "../src/util"
+
+describe("materials module", () => {
+  it("should export the set of materials", () => {
+    materials.set.should.be.an.instanceOf(Set)
+    materials.set.size.should.eql(52)
+    for (let material of materials.set) material.should.be.an.Object().with.properties("name", "symbol", "group", "value")
+  })
+  it("should have immutable members", () => {
+    for (let material of materials.set) {
+      (() => material.foo = "bar").should.throw()
+    }
+  })
+  it("should export the array of materials", () => {
+    materials.array.should.be.an.instanceOf(Array)
+    materials.array.length.should.eql(52)
+    for (let material of materials.array) material.should.be.an.Object().with.properties("name", "symbol", "group", "value")
+  })
+  it("should have no duplicate entries", () => {
+    let names = [...materials.set].map(el => el.name)
+    let symbols = [...materials.set].map(el => el.symbol)
+    names.filter(util.unique).length.should.eql(names.length)
+    symbols.filter(util.unique).length.should.eql(symbols.length)
+  }),
+  describe("materials.groups", () => {
+    it("should have a list of groups", () => {
+      materials.groups.forEach(group => group.should.be.a.String())
+    })
+    it("should be immutable", () => {
+      (() => materials.groups.push("foo")).should.throw()
+    })
+  })
+  describe("materials.find", () => {
+    it("should be a function", () => {
+      materials.find.should.have.type("function")
+    })
+    it("should find an material by field", () => {
+      materials.find("Au", "symbol").name.should.equal("Gold")
+      materials.find("Gold", "name").symbol.should.equal("Au")
+    })
+    it("should search material symbols by default", () => {
+      materials.find("Au").name.should.equal("Gold")
+    })
+    it("should be case-insensitive", () => {
+      materials.find("AU").name.should.equal("Gold")
+      materials.find("au").name.should.equal("Gold")
+      materials.find("aU").name.should.equal("Gold")
+    })
+    it("should return the unknown material when nothing is found", () => {
+      materials.find("Fake").name.should.equal("Unknown")
+    })
+  })
+  describe("materials.filter", () => {
+    it("should filter materials by field", () => {
+      materials.filter("Fe", "symbol").map(el => el.name).should.deepEqual([
+        "Ferrite Dust"
+      ])
+    })
+    it("should search material groups by default", () => {
+      materials.filter("Localized Mineral").length.should.eql(6)
+      for (let material of materials.filter("Localized Mineral")) {
+        material.group.should.eql("Localized Mineral")
+      }
+    })
+    it("should be case-insensitive", () => {
+      materials.filter("lOCALizeD mINeRaL", "group").length.should.eql(6)
+    })
+  })
+  describe("materials.any", () => {
+    it("should search for fields with any of a list of values", () => {
+      materials.any(["Fuel", "Mineral", "Stellar"], "group").length.should.eql(17)
+      for (let material of materials.any(["Fuel", "Mineral", "Goop"], "group")) {
+        ["Fuel", "Mineral"].indexOf(material.group).should.not.eql(-1)
+      }
+      materials.any(["Gold", "Silver", "Platinum", "Iron"], "name").length.should.eql(3)
+    })
+    it("should search material symbols by default", () => {
+      materials.any(["Fe", "Fe+", "Fe++", "Pb"]).map(el => el.name).should.deepEqual([
+        "Ferrite Dust", "Pure Ferrite", "Magnetized Ferrite"
+      ])
+    })
+    it("should be case-insensitive", () => {
+      materials.any(["gold", "SILVER", "pLATinUM", "HeLIum"], "name").length.should.eql(3)
+    })
+  })
+  describe("materials metadata", () => {
+    it("should expose a generated immutable list of material groups", () => {
+      materials.groups.should.be.an.Array()
+      ;(() => materials.groups.push("foo")).should.throw()
+      materials.groups.includes("Fuel").should.be.true()
+      materials.groups.filter(util.unique).length.should.eql(materials.groups.length)
+      // let's not bother testing all the groups since they may change
+    })
+    it("should expose a generated immutable list of material names", () => {
+      materials.names.should.be.an.Array()
+      ;(() => materials.names.push("foo")).should.throw()
+      materials.names.includes("Gold").should.be.true()
+      materials.names.filter(util.unique).length.should.eql(materials.names.length)
+      // let's not bother testing all the names since they may change
+    })
+    it("should expose a generated immutable list of material symbols", () => {
+      materials.symbols.should.be.an.Array()
+      ;(() => materials.symbols.push("foo")).should.throw()
+      materials.symbols.includes("Au").should.be.true()
+      materials.symbols.filter(util.unique).length.should.eql(materials.symbols.length)
+      // let's not bother testing all the symbols since they may change
+    })
+  })
+})
